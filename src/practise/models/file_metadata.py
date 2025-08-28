5b355be29afdf86d2831569a5ae00d86b56a9b25
Yunish <111888546+Itsyunish@users.noreply.github.com>
2025-08-28 11:02:57 +0545

    Delete src/practise/models/file_metadata.py

--- src/practise/models/file_metadata.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class FileMetadata(BaseModel):
-    filename: str
-    file_type: str
-    size_bytes: int
-    num_documents: int
-    num_chunks: int
-    splitter: str
